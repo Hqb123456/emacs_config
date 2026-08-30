e24ca3f6b1beb7b47151e9a4c7c84a0e034707af
Hqb123456 <94301495@qq.com>
2026-08-30 14:37:24 +0800

    Stop tracking Emacs cache and temporary files

--- transient/history.el
+++
@@ -1,9 +0,0 @@
-((magit-commit
-  ("--verbose"))
- (magit-diff
-  ("--stat" "--no-ext-diff"))
- (magit-log
-  ("--decorate" "--graph" "-n256"))
- (magit-merge nil)
- (magit-pull nil)
- (magit-push nil))
