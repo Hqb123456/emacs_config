283fb4133c20714b48ecdf06ed18f98c52837f9b
Hqb123456 <94301495@qq.com>
2026-08-30 21:44:01 +0800

    添加Iosevka Nerd Font Mono字体，允许部分多光标命令执行

--- font-config.el
+++ font-config.el
@@ -1,6 +1,6 @@
 ;; 由 my-save-font-config 自动生成
 (set-face-attribute 'default nil
-                    :family "JetBrainsMono Nerd Font Mono"
+                    :family "Iosevka Nerd Font Mono"
                     :height 173)
-(setq my-current-font-family "JetBrainsMono Nerd Font Mono"
+(setq my-current-font-family "Iosevka Nerd Font Mono"
       my-current-font-size 17)
